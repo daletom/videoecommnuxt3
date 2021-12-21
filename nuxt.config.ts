717d7821c12c6cc5462baf131a648f7acd166eda
Tom Dale <thomas.dale2@gmail.com>
2021-12-21 11:51:29 -0800

    removing preset node

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,9 +3,6 @@ import { defineNuxtConfig } from 'nuxt3'
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
     css: ["@/assets/css/styles.css"],
-    nitro: {
-        preset: 'node'
-    },
     build: {
         postcss: {
             postcssOptions: require("./postcss.config.js"),
