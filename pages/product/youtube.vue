b97af6c0cd6e988ea9ca9a643fc5802a431d2841
Tom Dale <thomas.dale2@gmail.com>
2022-01-06 14:36:42 -0800

    add product pages with videos

--- pages/product/youtube.vue
+++ pages/product/youtube.vue
@@ -320,16 +320,19 @@
         </div>
       <section aria-labelledby="details-heading">
         <div class="flex flex-col items-center text-center">
-          <h2 id="details-heading" class="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">Using MP4 Videos</h2>
+          <h2 id="details-heading" class="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">Using Youtube Videos</h2>
           <p class="mt-3 max-w-3xl text-lg text-gray-600">Our patented padded snack sleeve construction protects your favorite treats from getting smooshed during all-day adventures, long shifts at work, and tough travel schedules.</p>
         </div>
 
         <div class="mt-16 grid grid-cols-1 gap-y-16 lg:grid-cols-2 lg:gap-x-8">
           <div>
             <div class="w-full aspect-w-3 aspect-h-2 rounded-lg overflow-hidden">
-              <video controls loop class="w-full">
-                <source src="https://tom.imgix.net/imgix_unwrapping.mp4" type="video/mp4">
-              </video>
+              <iframe
+                class="modest-vid-embed__item w-full h-full"
+                src="https://www.youtube.com/embed/1HyeRonCz7I?mute=1&autoplay=0&modestbranding=1&loop=1&rel=0&amp;controls=1&amp;showinfo=0&playlist=1HyeRonCz7I"
+                frameborder="0"
+                allowfullscreen
+              >
             </div>
             <p class="mt-8 text-base text-gray-500">The 20L model has enough space for 370 candy bars, 6 cylinders of chips, 1220 standard gumballs, or any combination of on-the-go treats that your heart desires. Yes, we did the math.</p>
           </div>
